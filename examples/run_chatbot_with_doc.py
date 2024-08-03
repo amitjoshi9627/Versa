05586805d136443f0b1a253334b21ca667addaf9
Amit Joshi <amit@Amits-MacBook-Pro.local>
2024-08-03 17:56:56 +0530

    Updated Examples with chat bot type

--- examples/run_chatbot_with_doc.py
+++ examples/run_chatbot_with_doc.py
@@ -1,8 +1,9 @@
-from chatbot.constants import PDF_FILE_PATH
+from chatbot.constants import PDF_FILE_PATH, DOCBOT
 from chatbot.engine import ChatbotEngine
 
 if __name__ == "__main__":
-    chat_bot = ChatbotEngine(file_path=PDF_FILE_PATH)
+    # `chatbot_type` is `Docbot` to run chatbot with doc
+    chat_bot = ChatbotEngine(chatbot_type=DOCBOT, file_path=PDF_FILE_PATH)
     question = "What are some common threats to Environment?"
-    response = chat_bot.get_response(query=question)
-    print(f"\nQuestion: {question}\nResponse: {response}")
+    response_message = chat_bot.get_response(query=question)
+    print(f"\nQuestion: {response_message.query}\nResponse: {response_message.response}")
--- examples/run_simple_chatbot.py
+++ examples/run_simple_chatbot.py
@@ -1,7 +1,9 @@
+from chatbot.constants import DEFAULT
 from chatbot.engine import ChatbotEngine
 
 if __name__ == "__main__":
-    chat_bot = ChatbotEngine()
+    # Available `chatbot_type` - {'Therapist', 'Comedian', 'Default', 'Child', 'Expert'}
+    chat_bot = ChatbotEngine(chatbot_type=DEFAULT)
     question = "What is 2/2?"
-    response = chat_bot.get_response(query=question)
-    print(f"\nQuestion: {question}\nResponse: {response}")
+    response_message = chat_bot.get_response(query=question)
+    print(f"\nQuestion: {response_message.query}\nResponse: {response_message.response}")
