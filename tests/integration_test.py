825738b84ad27675a9a300447e9514e1e7c173e5
Amit Joshi <amit@Amits-MacBook-Pro.local>
2024-08-14 15:31:17 +0530

    [Update] Updated tests with latest changes in Engine class

--- tests/integration_test.py
+++ tests/integration_test.py
@@ -1,20 +1,31 @@
-from typing import Optional
-
 import pytest
 
-from chatbot.engine import ChatbotEngine
+from chatbot.engine import ChatbotEngine, DocBotEngine
 from chatbot.utils import ResponseMessage
-from tests.constants import CHILD, DOCBOT, TEST_PDF_FILE_PATH
+from tests.constants import CHILD, COMEDIAN, TEST_PDF_FILE_PATH
 
 
-def get_chatbot_engine(chatbot_type: str, file_path: Optional[str] = None) -> ChatbotEngine:
-    return ChatbotEngine(chatbot_type, file_path)
+def get_chatbot_engine(chatbot_type: str) -> ChatbotEngine:
+    return ChatbotEngine(chatbot_type)
 
 
-@pytest.mark.parametrize("chatbot_type, file_path", [(CHILD, None), (DOCBOT, TEST_PDF_FILE_PATH)])
-def test_chatbot_engine_integration(chatbot_type: str, file_path: Optional[str]) -> None:
-    chatbot_engine = get_chatbot_engine(chatbot_type, file_path)
-    response_message = chatbot_engine.get_response("What is a blue whale?")
+def get_docbot_engine(file_path: str) -> DocBotEngine:
+    return DocBotEngine(file_path)
+
+
+@pytest.mark.parametrize("chatbot_type", [CHILD, COMEDIAN])
+def test_chatbot_engine_integration(chatbot_type: str) -> None:
+    chatbot_engine = get_chatbot_engine(chatbot_type)
+    response_message = chatbot_engine.get_response("What is a 2/2?")
+    assert isinstance(response_message, ResponseMessage)
+    assert isinstance(response_message.response, str)
+    assert len(response_message.response) > 0
+
+
+@pytest.mark.parametrize("file_path", [TEST_PDF_FILE_PATH])
+def test_docbot_engine_integration(file_path: str) -> None:
+    docbot_engine = get_docbot_engine(file_path)
+    response_message = docbot_engine.get_response("What is a blue whale?")
     assert isinstance(response_message, ResponseMessage)
     assert isinstance(response_message.response, str)
     assert len(response_message.response) > 0
