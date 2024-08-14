92f55c328c21a11030b7e08d7973bc260d73a524
Amit Joshi <amit@Amits-MacBook-Pro.local>
2024-08-14 15:26:30 +0530

    Changed file name + Engine code

--- examples/run_chatbot.py
+++ examples/run_chatbot.py
@@ -3,6 +3,7 @@ from chatbot.engine import ChatbotEngine
 
 if __name__ == "__main__":
     # Available `chatbot_type` - {'Therapist', 'Comedian', 'Default', 'Child', 'Expert'}
+
     chat_bot = ChatbotEngine(chatbot_type=DEFAULT)
     question = "What is 2/2?"
     response_message = chat_bot.get_response(query=question)
--- examples/run_docbot.py
+++ examples/run_docbot.py
@@ -1,9 +1,8 @@
-from chatbot.constants import PDF_FILE_PATH, DOCBOT, CHAT_SEPARATOR
-from chatbot.engine import ChatbotEngine
+from chatbot.constants import PDF_FILE_PATH, CHAT_SEPARATOR
+from chatbot.engine import DocBotEngine
 
 if __name__ == "__main__":
-    # `chatbot_type` is `Docbot` to run chatbot with doc
-    chat_bot = ChatbotEngine(chatbot_type=DOCBOT, file_path=PDF_FILE_PATH)
+    chat_bot = DocBotEngine(file_path=PDF_FILE_PATH)
     question = "What is the typical size of a blue whale?"
     response_message = chat_bot.get_response(query=question)
     print(
