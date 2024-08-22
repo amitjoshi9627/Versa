50aa4450c7eff4be71e6b203328899ca42ab9ff9
Amit Joshi <amit@Amits-MacBook-Pro.local>
2024-08-22 10:49:13 +0530

    Updated with the latest exposed arguments change

--- examples/run_chatbot.py
+++ examples/run_chatbot.py
@@ -1,10 +1,15 @@
-from chatbot.constants import DEFAULT, CHAT_SEPARATOR
+from chatbot.constants import DEFAULT, CHAT_SEPARATOR, LLM_MODEL, CREATIVE_LLM_TEMP
 from chatbot.engine import ChatBotEngine
 
 if __name__ == "__main__":
     # Available `chatbot_type` - {'Therapist', 'Comedian', 'Default', 'Child', 'Expert'}
 
-    chat_bot = ChatBotEngine(chatbot_type=DEFAULT)
+    chat_bot = ChatBotEngine(
+        chatbot_type=DEFAULT,
+        model_name_or_path=LLM_MODEL,
+        quantize=False,
+        llm_temp=CREATIVE_LLM_TEMP,
+    )
     question = "What is 2/2?"
     response_message = chat_bot.get_response(query=question)
     print(
--- examples/run_docbot.py
+++ examples/run_docbot.py
@@ -1,10 +1,15 @@
-from chatbot.constants import PDF_FILE_PATH, CHAT_SEPARATOR
+from chatbot.constants import PDF_FILE_PATH, CHAT_SEPARATOR, LLM_MODEL, DETERMINISTIC_LLM_TEMP
 from chatbot.engine import DocBotEngine
 
 if __name__ == "__main__":
-    chat_bot = DocBotEngine(file_path=PDF_FILE_PATH)
+    doc_bot = DocBotEngine(
+        file_path=PDF_FILE_PATH,
+        model_name_or_path=LLM_MODEL,
+        quantize=False,
+        llm_temp=DETERMINISTIC_LLM_TEMP,
+    )
     question = "What is the typical size of a blue whale?"
-    response_message = chat_bot.get_response(query=question)
+    response_message = doc_bot.get_response(query=question)
     print(
         f"{CHAT_SEPARATOR}Question: {response_message.query}{CHAT_SEPARATOR}Response: {response_message.response}"
     )
