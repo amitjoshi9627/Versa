1a5e9d360c38faf572827093909ecde0240f9148
Amit Joshi <amit@Amits-MacBook-Pro.local>
2024-08-27 15:59:04 +0530

    [UPDATE] Engine examples

--- examples/run_chatbot.py
+++ examples/run_chatbot.py
@@ -1,16 +1,17 @@
-from chatbot.constants import DEFAULT, CHAT_SEPARATOR, LLM_MODEL, CREATIVE_LLM_TEMP
+from chatbot.constants import DEFAULT, CHAT_SEPARATOR, EXAMPLE_LLM_MODEL, CREATIVE_LLM_TEMP
 from chatbot.engine import ChatBotEngine
 
 if __name__ == "__main__":
     # Available `chatbot_type` - {'Therapist', 'Comedian', 'Default', 'Child', 'Expert'}
 
+    # Use access token for gated huggingface models
     chat_bot = ChatBotEngine(
         chatbot_type=DEFAULT,
-        model_name_or_path=LLM_MODEL,
+        model_name_or_path=EXAMPLE_LLM_MODEL,
         quantize=False,
         llm_temp=CREATIVE_LLM_TEMP,
     )
-    question = "What is 2/2?"
+    question = "What is the greatest life lesson?"
     response_message = chat_bot.get_response(query=question)
     print(
         f"{CHAT_SEPARATOR}Question: {response_message.query}{CHAT_SEPARATOR}Response: {response_message.response}"
